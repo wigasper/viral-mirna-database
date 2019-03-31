7c58294e47126fd5c5ead527df90c67444c69cbe
Kirk Gasper <wigasper@gmail.com>
2019-03-31 10:02:48 -0500

    added table creation skeleton

---
+++ createTables.sql
@@ -0,0 +1,29 @@
+CREATE TABLE ViralmiRNA (
+
+
+);
+
+CREATE TABLE ViralTarget (
+
+
+):
+
+CREATE TABLE Protein (
+
+
+);
+
+CREATE TABLE Interaction (
+
+
+);
+
+CREATE TABLE Annotation (
+
+
+);
+
+CREATE TABLE GOTerm (
+
+
+);
