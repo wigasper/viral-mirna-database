cbdf00a14458a9de2d09c77cc4152f5bcecc3d50
Kirk Gasper <wigasper@gmail.com>
2019-04-06 10:18:22 -0500

    fixed remove header mistake

--- prep_data.sh
+++ prep_data.sh
@@ -1,15 +1,12 @@
 # Remove unneeded columns from the viral miRNA data for the viral-miRNA table
-cut -f 1,2,5  ./data/vmt.tsv > ./data/viral_mirna_data.tsv
-# Remove header line
-tail -n 7283 ./data/viral_mirna_data.tsv > ./data/viral_mirna_data.tsv
+# and remove header line
+cut -f 1,2,5  ./data/vmt.tsv | tail -n 7283 > ./data/viral_mirna_data.tsv
 
 # Remove unneed columns from the BioGRID data
 cut -f 2,3,8,9 ./data/biogrid_9606_3.5.169.tsv > ./data/protein_interaction_data.tsv
 
-# Get data for the viral-target table
-cut -f 1,7  ./data/vmt.tsv > ./data/viral_target_data.tsv
-# Remove header line
-tail -n 7283 ./data/viral_target_data.tsv > ./data/viral_target_data.tsv
+# Get data for the viral-target table and remove header line
+cut -f 1,7  ./data/vmt.tsv | tail -n 7283 > ./data/viral_target_data.tsv
 
 # Remove GO terms from protein data, these will only be needed in the 
 # Annotates table
