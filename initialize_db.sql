234a15e799a03204b56bc9b68d7c316f69c8a92e
Kirk Gasper <wigasper@gmail.com>
2019-04-06 11:38:46 -0500

    moved table creation to init

--- initialize_db.sql
+++ initialize_db.sql
@@ -1,5 +1,41 @@
 /* Create tables */
-\i /media/wkg/storage/db-final-project/create_tables.sql
+CREATE TABLE viral_miRNA (
+    vi_miRNA_id             varchar,        -- Unique viral miRNA ID
+    virus                   varchar,        -- The virus associated with the viral miRNA
+    miRNA_symbol            varchar         -- The miRNA symbol of the viral miRNA
+);
+
+CREATE TABLE viral_target (
+    vi_miRNA_id             varchar,        -- Unique viral miRNA ID
+    uniprot_id              varchar         -- Unique UniProt protein ID
+);
+
+CREATE TABLE protein (
+    uniprot_id              varchar,        -- Unique UniProt protein ID
+    symbol                  varchar,        -- Gene symbol
+    full_name               varchar,        -- Full name of the gene
+    gene_id                 int,            -- Entrez Gene ID for the gene
+    tissue                  varchar         -- Tissue specificity for the gene
+);
+
+CREATE TABLE protein_interaction (
+    interactor_a_gene_id    int,            -- Entrez Gene ID for interactor A
+    interactor_b_gene_id    int,            -- Entrez Gene ID for interactor B
+    interactor_a_symbol     varchar,        -- Gene symbol for interactor A
+    interactor_b_symbol     varchar         -- Gene symbol for interactor B
+);
+
+CREATE TABLE annotates (
+    uniprot_id              varchar,        -- Unique UniProt protein ID
+    go_id                   varchar         -- Unique GO ID for the term applied to the gene
+
+);
+
+CREATE TABLE goterm (
+    go_id                   varchar,        -- Unique GO ID for the term
+    term                    varchar,        -- Natural language description of the term
+    term_namespace          varchar         -- Term namespace
+);
 
 /* Copy over data */
 \copy viral_miRNA FROM '/media/wkg/storage/db-final-project/data/viral_mirna_data.tsv' WITH NULL AS '-'
