a6063c7026001f9074b62324d2280f9e89528fdb
Kirk Gasper <wigasper@gmail.com>
2019-04-06 10:20:19 -0500

    removed PPI header line

--- prep_data.sh
+++ prep_data.sh
@@ -2,8 +2,8 @@
 # and remove header line
 cut -f 1,2,5  ./data/vmt.tsv | tail -n 7283 > ./data/viral_mirna_data.tsv
 
-# Remove unneed columns from the BioGRID data
-cut -f 2,3,8,9 ./data/biogrid_9606_3.5.169.tsv > ./data/protein_interaction_data.tsv
+# Remove unneed columns from the BioGRID data and remove header
+cut -f 2,3,8,9 ./data/biogrid_raw.tsv | tail -n 468059 > ./data/protein_interaction_data.tsv
 
 # Get data for the viral-target table and remove header line
 cut -f 1,7  ./data/vmt.tsv | tail -n 7283 > ./data/viral_target_data.tsv
