d2efbee6da2371a9112a92e7733ad8bdbc5b9050
Kirk Gasper <wigasper@gmail.com>
2019-04-12 10:20:49 -0500

    added random step param search

--- network_clustering.R
+++ network_clustering.R
@@ -10,18 +10,32 @@ graph <- simplify(graph)
 
 #plot(graph)
 
-# plot(graph, vertex.label=NA)
-
-start <- Sys.time()
+# Walktrap testing, here I am looking for the steps value that minimizes
+# the number of communities (to a certain extent)
+steps <- c(20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80)
+results <- data.frame()
+names(results) <- c("steps", "communities", "time")
+for (step in steps)
+{
+  start <- Sys.time()
+  communities <- cluster_walktrap(graph, weights=NULL, steps=step)
+  results <- rbind(results, data.frame(steps=step, communities=length(communities), time=(Sys.time - start)))
+}
+system("python3 /media/wkg/storage/bender/discordnotifier.py -m ppi_clustering_done")
+#start <- Sys.time()
 #communities <- cluster_fast_greedy(graph, weights=NULL)
 #maybe edit walktrap steps???
-communities <- cluster_walktrap(graph, weights=NULL)
+# 6 steps has lowest num of communities right now
+#communities <- cluster_walktrap(graph, weights=NULL, steps=35)
 #communities <- cluster_edge_betweenness(graph, weights=NULL)
-system("python3 /media/wkg/storage/bender/discordnotifier.py -m ppi_clustering_done")
-Sys.time() - start
+#system("python3 /media/wkg/storage/bender/discordnotifier.py -m ppi_clustering_done")
+#Sys.time() - start
 
 #plot(communities, graph)
 
 V(graph)$community <- communities$membership
-colors <- palette(terrain.colors(1024, alpha=.6))
+colors <- palette(terrain.colors(length(communities), alpha=.6))
 plot(graph, vertex.color=colors[V(graph)$community], vertex.label=NA)
+
+# get membership values for nodes
+membership <- cbind(V(graph)$name, communities$membership)
