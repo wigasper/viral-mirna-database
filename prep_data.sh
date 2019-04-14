ce7b10d15b577c45a8f1e3aa958df40a6f7b0f96
Kirk Gasper <wigasper@gmail.com>
2019-04-14 16:40:12 -0500

    removed unneeded data for other viruses

--- prep_data.sh
+++ prep_data.sh
@@ -1,12 +1,13 @@
+#!/usr/bin/env bash
 # Remove unneeded columns from the viral miRNA data for the viral-miRNA table
-# and remove header line
-cut -f 1,2,5  ./data/vmt.tsv | tail -n 7283 | grep 'EBV\|KSHV' > ./data/viral_mirna_data.tsv
+# and viruses other than EBV and KSHV
+cut -f 1,2,5  ./data/vmt.tsv | grep 'EBV\|KSHV' > ./data/viral_mirna_data.tsv
 
 # Remove unneed columns from the BioGRID data and remove header
 cut -f 2,3,8,9 ./data/biogrid_raw.tsv | tail -n 468058 > ./data/protein_interaction_data.tsv
 
 # Get data for the viral-target table and remove header line
-cut -f 1,7  ./data/vmt.tsv | tail -n 7283 > ./data/viral_target_data.tsv
+grep 'EBV\|KSHV' ./data/vmt.tsv | cut -f 1,7 > ./data/viral_target_data.tsv
 
 # Remove GO terms from protein data, these will only be needed in the 
 # Annotates table
