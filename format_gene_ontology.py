07534662dc1f537feb97467d702fa5b4a1c1325a
William Gasper <wigasper@gmail.com>
2019-08-22 18:59:54 -0500

    added argparse

--- format_gene_ontology.py
+++ format_gene_ontology.py
@@ -1,34 +1,37 @@
 #!/usr/bin/env python3
+import argparse
 
-# A list to store GO terms
-go_terms = []
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="The Gene Ontology in OBO format",
+            required=True, type=str)
+    args = parser.parse_args()
 
-# A single term
-term = []
+    # A list to store GO terms
+    go_terms = []
 
-# Open the ontology text file and parse
-with open("./data/go.obo", "r") as go:
-    for line in go:
-        if line.startswith("[Term]"):
-            if term:
-                go_terms.append(term)
-            term = []
-        if line.startswith("id: "):
-            term.append(line[4:].strip("\n"))
-        if line.startswith("name: "):
-            term.append(line[6:].strip("\n"))
-        if line.startswith("namespace: "):
-            term.append(line[11:].strip("\n"))
+    # A single term
+    term = []
 
-# Append final term
-go_terms.append(term)
+    # Open the ontology text file and parse
+    with open(args.input, "r") as go:
+        for line in go:
+            if line.startswith("[Term]"):
+                if term:
+                    go_terms.append(term)
+                term = []
+            if line.startswith("id: "):
+                term.append(line[4:].strip("\n"))
+            if line.startswith("name: "):
+                term.append(line[6:].strip("\n"))
+            if line.startswith("namespace: "):
+                term.append(line[11:].strip("\n"))
 
-# Write as a tab-delimited file for insertion into the DB
-with open("./data/go.tab", "w") as out:
-    for term in go_terms:
-        out.write(term[0])
-        out.write("\t")
-        out.write(term[1])
-        out.write("\t")
-        out.write(term[2])
-        out.write("\n")
+    # Append final term
+    go_terms.append(term)
+
+    # Write as a tab-delimited file for insertion into the DB
+    with open("./data/go.tab", "w") as out:
+        for term in go_terms:
+            out.write("\t".join([term[0], term[1], term[2]]))
+            out.write("\n")
